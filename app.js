03cefd5184ada4eddec07b2f90183e1a2ac7771e
sahandbabali <sahand.babali@gmail.com>
2021-10-21 18:15:51 +0330

    progress bar working

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var fliphistory = [];
-var headsount = 0;
+var headscount = 0;
 var tailscount = 0;
 var totalflips = 0;
 
@@ -7,13 +7,15 @@ document.getElementById("flipbutton").addEventListener("click", flipit);
 
 function flipit() {
   let randombinaryvalue = Math.round(Math.random());
+  totalflips++;
   console.log(randombinaryvalue);
 
   if (randombinaryvalue == 0) {
     // change the coin pic to 0
     document.getElementById("coinimg").src = "coin128-0.png";
     document.getElementById("flipresult").innerHTML = "Head";
-    headsount++;
+    headscount++;
+    updatebadges();
     // update the progress bar
     updateprogressbar();
     // add to table
@@ -21,9 +23,9 @@ function flipit() {
   } else if (randombinaryvalue == 1) {
     // change the coin pic to 1
     document.getElementById("coinimg").src = "coin128-1.png";
-    document.getElementById("flipresult").innerHTML = "tail";
-
+    document.getElementById("flipresult").innerHTML = "Tail";
     tailscount++;
+    updatebadges();
     // update the progress bar
     updateprogressbar();
     // add to table
@@ -32,8 +34,38 @@ function flipit() {
 }
 
 function updateprogressbar() {
-  // update count badges
   // update the progress bar
+  var headspercent = (headscount * 100) / totalflips;
+  headspercent = Math.round(headspercent, 0);
+
+  if (headspercent == NaN) {
+    headspercent == 0;
+  }
+  if (headspercent == Infinity) {
+    headspercent == 100;
+  }
+
+  console.log(`headspercent ${headspercent}`);
+  document.getElementById("headsbar").style.width = `${headspercent}%`;
+  document.getElementById("headsbar").innerHTML = headspercent;
+
+  var tailspercent = (tailscount * 100) / totalflips;
+  tailspercent = Math.round(tailspercent, 0);
+  if (tailspercent == NaN) {
+    tailspercent == 0;
+  }
+  if (tailspercent == Infinity) {
+    tailspercent == 100;
+  }
+
+  console.log(`tailspercent ${tailspercent}`);
+  document.getElementById("tailsbar").style.width = `${tailspercent}%`;
+  document.getElementById("tailsbar").innerHTML = tailspercent;
+}
+
+function updatebadges() {
+  document.getElementById("headsbadge").innerHTML = `${headscount}`;
+  document.getElementById("tailsbadge").innerHTML = `${tailscount}`;
 }
 
 function addtotable(lastflip) {}
