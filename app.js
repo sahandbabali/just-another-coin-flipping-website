f580a64fb2f75284498d5de4afe7d82b9a0c42ff
sahandbabali <sahand.babali@gmail.com>
2021-10-21 17:45:40 +0330

    basic flip added

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ var headsount = 0;
 var tailscount = 0;
 var totalflips = 0;
 
+document.getElementById("flipbutton").addEventListener("click", flipit);
+
 function flipit() {
   let randombinaryvalue = Math.round(Math.random());
   console.log(randombinaryvalue);
 
   if (randombinaryvalue == 0) {
     // change the coin pic to 0
-
+    document.getElementById("coinimg").src = "coin128-0.png";
+    document.getElementById("flipresult").innerHTML = "Head";
     headsount++;
     // update the progress bar
     updateprogressbar();
@@ -17,6 +20,8 @@ function flipit() {
     addtotable(randombinaryvalue);
   } else if (randombinaryvalue == 1) {
     // change the coin pic to 1
+    document.getElementById("coinimg").src = "coin128-1.png";
+    document.getElementById("flipresult").innerHTML = "tail";
 
     tailscount++;
     // update the progress bar
